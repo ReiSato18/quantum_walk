63c06dbf473e34db04a8f97b03afc8343c97d424
Rei <36851996+ReiSato18@users.noreply.github.com>
2018-05-07 18:12:26 +0900

    Update and rename 量子ウォーク2(2018 0506）.py to quantum-walk.py

--- quantum-walk.py
+++ quantum-walk.py
@@ -4,38 +4,30 @@ import math
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.animation as animation
 
-#環境設定
+#set the environment
 n=300
 theta= 10*(math.pi)/12
 
-P = [[np.cos(theta),np.sin(theta)],[0,0]]
-Q = [[0,0],[np.sin(theta),-np.cos(theta)]]
-x_list=[]#xline
+P = [[np.cos(theta),np.sin(theta)],[0,0]] #Shift Operator 
+Q = [[0,0],[np.sin(theta),-np.cos(theta)]] #Shift Operator
+x_list=[]#xline  
 t_list=[]#time
 p_list=[]#probability
 s_list=[]#state
 
-for x in range(0,2*n+1):
-    if x == n:
+for x in range(0,2*n+1): #you can arange the range you want
+    if x == n:           #the start 
         phai = [1, 0]
     else:
         phai = [0,0]
     p = np.dot(phai,phai)
 
-    x_list.append(x)
-    s_list.append(phai)
-    p_list.append(p)
+    x_list.append(x)    #[0,1,2,3,4,.......,n,n+1,.....2*n+1]
+    s_list.append(phai) #[[0,0],[0,0],..........,[1,0],[0,0],....[0,0]]
+    p_list.append(p)    #[0,0,0,0,0,0...........1,......0,0,0,0...]
 
-#def p(x_list):
-        #p=np.inner(x_list[x],x_list[x])
-        #return p
 
-#p_list.append(p(x_list))
-    #print(x,end='')
-#t =0
-#t=0で原点の値(python上では5番目の配列)は[1 0]
-
-for t in range(0,2*n):
+for t in range(0,2*n): #calculate the probability depend on time.
     t_list.append(t)
     if t ==0:
         s_list
@@ -58,7 +50,6 @@ for t in range(0,2*n):
                 #plt.pause(0.1)
 
     #2Dplot
-    #fig = plt.figure()
     print(x_list,p_list)
     #plt.bar(x_list,p_list,width=0.1,color='black')
     plt.xlabel("x")
@@ -68,22 +59,3 @@ for t in range(0,2*n):
     plt.plot(x_list,p_list,color="red",linewidth=0.5)
     plt.pause(0.01)
     plt.cla()
-
-    #ims=[]
-    #im = plt.plot(x_list,p_list)
-    #ims.append(im)
-    #ani = animation.ArtistAnimation(fig,ims, interbal=1)
-    #plt.show()
-
-
-
-    #3Dplot
-    #fig=plt.figure()
-    #ax =Axes3D(fig)
-    #fig.add_subplot(111, projection="3d")
-    #ax.bar(x_list, t_list, p_list, color="black")
-    #ax.set_xlabel("x")
-    #ax.set_ylabel("t")
-    #ax.set_zlabel("probability")
-
-    #plt.show()
