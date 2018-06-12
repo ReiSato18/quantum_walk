6de923cff7b06cab1b8df4374255332ec2d16336
Rei <36851996+ReiSato18@users.noreply.github.com>
2018-06-12 21:57:52 +0900

    Add files via upload

---
+++ src/1次元量子ウォークとランダムウォーク.py
@@ -0,0 +1,90 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.animation as animation
+
+#環境設定
+n=100 #x軸
+m=100 #t
+theta = 3*(math.pi)/12
+
+P = [[np.cos(theta),np.sin(theta)],[0,0]]
+Q = [[0,0],[np.sin(theta),-np.cos(theta)]]
+x_list=[]#xline
+t_list=[]#time
+p_list=[]#probability
+s_list=[]#state
+a = 1/math.sqrt(2)
+b = 1j/math.sqrt(2)
+
+#ランダムウォーク
+R=1/2
+L=1/2
+X_list=[]
+P_list=[]
+
+#quantumwalk
+for j in range(0,2*n+1):
+    if j  == n:
+        phai = [a ,b]
+        pro = 1
+    else:
+        phai = [0,0]
+        pro =0
+    p = np.dot(phai,np.conj(phai))
+
+    x_list.append(j)
+    X_list.append(j)
+    s_list.append(phai)
+    p_list.append(p)
+    P_list.append(pro)
+
+
+
+for t in range(0,m+1):
+    t_list.append(t)
+    if t ==0:
+        s_list
+        p_list
+        P_list
+    else:
+        next_s_list = [0]*len(s_list)
+        next_P_list = [0]*len(P_list) #listと同じ要素の数ですべて0を用意（初期化）
+        for i in range(0,2*n+1):
+            if i == 0:
+                next_s_list[i] = np.dot(P, s_list[i+1])
+                next_P_list[i] = P_list[i+1]*L
+            elif i == 2*n:
+                next_s_list[i] = np.dot(Q, s_list[i-1])
+                next_P_list[i] = P_list[i-1]*R
+            else:
+                next_s_list[i] = np.dot(P, s_list[i+1]) + np.dot(Q, s_list[i-1])
+                next_P_list[i] = P_list[i+1]*L + P_list[i-1]*R
+
+            p_list[i] = np.dot(next_s_list[i],np.conj(next_s_list[i]))
+        s_list = next_s_list
+        P_list = next_P_list
+
+
+
+    print(p_list,P_list)
+
+    plt.xlabel("x")
+    plt.ylabel("probability")
+    plt.ylim([0,0.1])
+    plt.xlim([-n,3*n])
+    plt.plot( x_list,np.real(p_list),color="red",linewidth=1.0,label="quantum walk")
+    plt.plot(X_list, P_list,color="blue",linewidth=1.0,label="random walk")
+    plt.legend(loc="best")
+    plt.pause(0.01)
+    plt.cla()
+
+#plt.ylim([0,0.1])
+#plt.xlim([-n,3*n])
+#plt.xlabel("x",fontsize=24)
+#plt.ylabel("probability",fontsize=24)
+#plt.plot(x_list,np.real(p_list),color="red",label="quantum walk",linewidth=0.7)
+#plt.plot(X_list,P_list,color="blue",label="random walk",linewidth=0.7)
+#plt.legend(title="t=100",loc="best",fontsize=10)
+#plt.show()
---
+++ src/1次元量子ウォーク改良.py
@@ -0,0 +1,85 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+
+n=4
+m=3
+theta=3*(math.pi)/12
+
+x_list=[]
+t_list=[]
+phi_list=[]
+init_phi=[]
+new_phi=[]
+p_list=[]
+
+for i in range(0,2*n+1):
+    a=[0,0]
+    new_phi.append(a)
+    phi_list.append(a)
+    #p_list.append(i)
+
+def xline():
+    for i in range(0,2*n+1):
+        x_list.append(i)
+    return x_list
+#print(xline()[n])
+
+def quantumcoin(theta):
+    C = [[np.cos(theta),-np.sin(theta)],[np.sin(theta),np.cos(theta)]]
+    return C
+#print(quantumcoin(3*(math.pi)/12))
+
+#t=0の時のphi状態
+def initPositionPhi():
+    phi = [1,0]
+    othersphi=[0,0]
+    for i in range(0,2*n+1):
+        if i == n:
+            init_phi.append(phi)
+        else:
+            init_phi.append(othersphi)
+    return init_phi
+#print(initPositionPhi())
+
+def CoinOperator(x,new_phi):
+    new_phi[x]=np.dot(quantumcoin(theta),new_phi[x])
+    return new_phi[x]
+print(CoinOperator(n,initPositionPhi()))
+
+#a=(CoinOperator(1,initPositionPhi()))[n][0]
+#print(a)
+
+#def ShiftOperator(steps,phi):
+for t in range(1,m+1):
+    #if t==0:
+        #initPositionPhi()
+    if t==1:
+        new_phi[n-t][1] = (CoinOperator(n,initPositionPhi()))[1]
+        new_phi[n+t][0] = (CoinOperator(n,initPositionPhi()))[0]
+        print(t,new_phi)
+    else:#t=2~3
+        new_phi[n-t] = (CoinOperator(n-(t-1),new_phi))[1] + (CoinOperator(n-(t+1),new_phi))[0]
+        new_phi[n+t] = (CoinOperator(n+(t-1),new_phi))[0] + (CoinOperator(n+(t+1),new_phi))[1]
+    print(t,new_phi)
+
+
+        #if x==0:
+            #new_phi[x+1][0]=(CoinOperator(x,new_phi[x]))[x][0]
+        #if x==2*n:
+            #new_phi[x-1][1]=(CoinOperator(x,new_phi[x]))[x][1]
+        #else:
+            #new_phi[x+1][0]=(CoinOperator(x,new_phi[x]))[x][0]
+            #new_phi[x-1][1]=(CoinOperator(x,new_phi[x]))[x][1]
+
+
+            #elif x==2*n:
+                #new_phi[n+t][0]=(CoinOperator(1,new_phi[n+(t-1)]))[n+(t-1)][0]
+                #new_phi[n-t][1]=(CoinOperator(1,new_phi[n-(t-1)]))[n-(t-1)][1]
+            #else:
+    #print(t,new_phi)
+    #print(t,final_phi)
+    #for x in range(0,2*n+1):
+        #final_phi[x]=np.dot(quantumcoin(theta),final_phi[x])
+    #return new_phi
+#print(final_phi)
