c380e13282bdafb44ebdc2fe11fbd238571887b5
Rei <36851996+ReiSato18@users.noreply.github.com>
2018-08-03 03:30:09 +0900

    Update 1次元2状態量子ウォーク.py

--- src/1次元2状態量子ウォーク.py
+++ src/1次元2状態量子ウォーク.py
@@ -1,68 +1,68 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import math
-from mpl_toolkits.mplot3d import Axes3D
-import matplotlib.animation as animation
+import itertools
 
-#環境設定
-n=400
+n=50
+step= 500
+l = 0
+#flip flop walk(Szegedy walk)
 theta = 3*(math.pi)/12
+H = np.array([[np.cos(theta),np.sin(theta)],[np.sin(theta),-np.cos(theta)]])
 
-P = [[np.cos(theta),np.sin(theta)],[0,0]]  #Shift Operator
-Q = [[0,0],[np.sin(theta),-np.cos(theta)]] #Shift Operator
-x_list=[]#xline
-t_list=[]#time
-p_list=[]#probability
-s_list=[]#state
-a = 1/math.sqrt(2)   #you can choose any number that is normalized with b
-b = 1j/math.sqrt(2)
+P = np.zeros((2,2)); P[1,:] = H[1,:]
+Q = np.zeros((2,2)); Q[0,:] = H[0,:]
+###å›ºå®šç«¯
+p = [[0,0],[1,0]]
+q = [[0,1],[0,0]]
+#######
+x_list=[ i for i in range(0,2*n+1)]
+t_list=[ i for i in range(0,step+1)]
 
-for x in range(0,2*n+1):
-    if x == n:
-        phai = [a ,b]
+p_list= np.zeros(2*n+1)
+phi_list = np.zeros((2*n+1,2),dtype="complex")
+next_phi_list = np.zeros((2*n+1,2),dtype="complex")
+
+phi_list[n] = [1 , 1j]/np.sqrt(2)
+p_list[n] = 1.0
+###########
+for t in range(0,step+1):
+    if t == 0:
+        pass
     else:
-        phai = [0,0]
-    p = np.dot(phai,np.conj(phai))
-
-    x_list.append(x)
-    s_list.append(phai)
-    p_list.append(p)
-
-
-for t in range(0,501):
-    t_list.append(t)
-    if t ==0:
-        s_list
-        p_list
-    else:
-        next_s_list = [0]*len(s_list)  ）
-        for x in range(0,2*n+1):
+        for x in x_list:
+            x1 = (x - 1)
+            x2 = (x + 1)
             if x == 0:
-                next_s_list[0] = np.inner(P, s_list[1])
-
+                next_phi_list[x] = np.array([np.dot(P,phi_list[x2])])
+            elif x == 1:
+                next_phi_list[x] = np.array([np.dot(P,phi_list[x2]) + np.dot(q,phi_list[x1])])
             elif x == 2*n:
-                next_s_list[2*n] = np.inner(Q, s_list[2*n-1])
-
+                next_phi_list[x] = np.array([np.dot(Q,phi_list[x1])])
+            elif x == 2*n-1:
+                next_phi_list[x] = np.array([np.dot(Q,phi_list[x1]) + np.dot(p,phi_list[x2])])
             else:
-                next_s_list[x] = np.inner(P, s_list[x+1]) + np.inner(Q, s_list[x-1])
-            p_list[x] = np.dot(next_s_list[x],np.conj(next_s_list[x]))
-        s_list = next_s_list
+                next_phi_list[x] = np.array([np.dot(P,phi_list[x2]) + np.dot(Q,phi_list[x1])])
+
+            p_list[x] = np.real(np.vdot(next_phi_list[x],next_phi_list[x]))
+        phi_list = np.copy(next_phi_list)
+    print(t,p_list.sum())
+    plt.xlabel("x",fontsize="24")
+    plt.ylabel("probability",fontsize="24")
+    plt.ylim([0,0.25])
+    plt.plot(x_list,p_list,color="blue",label="quantum walk",linewidth="1")
+    plt.legend(title="flip flop walk",loc="best",fontsize=10)
+    plt.tight_layout()
+    plt.pause(0.01)
+    plt.cla()
 
 
-    #if you want to watch the real time move of probability
-    #print(p_list)
-    #plt.xlabel("x")
-    #plt.ylabel("probability")
-    #plt.ylim([0,0.1])
-    #plt.xlim([-n,3*n])
-    #plt.plot(x_list,np.real(p_list),color="red",linewidth=0.3)
-    #plt.pause(0.01)
-    #plt.cla()
 
-#t=500 only as a figure
-plt.xlabel("x")
-plt.ylabel("probability")
-plt.ylim([0,0.1])
-plt.xlim([-n,3*n])
-plt.plot(x_list,np.real(p_list),color="red",linewidth=0.3)
-plt.show()
+########
+plt.xlabel("x",fontsize="24")
+plt.ylabel("probability",fontsize="24")
+#plt.ylim([0,0.25])
+plt.plot(x_list,p_list,color="red",label="quantum walk",linewidth="1")
+plt.legend(title="flip flop walk",loc="best",fontsize=10)
+plt.tight_layout()
+#plt.show()
